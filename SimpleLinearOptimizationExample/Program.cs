7592dfa49b851b42c3c837820a1dabf59c6cf940
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-08-10 10:40:32 +0200

    Dodanie przykładu optymalizacji

---
+++ SimpleLinearOptimizationExample/Program.cs
@@ -0,0 +1,75 @@
+﻿using Google.OrTools.LinearSolver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLinearOptimizationExample
+{
+
+    // https://developers.google.com/optimization/introduction/using#csharp
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunLinearExample("GLOP_LINEAR_PROGRAMMING");
+        }
+
+        private static void RunLinearExample(string solverType)
+        {
+            // * Declare the solver.
+            Solver solver = Solver.CreateSolver("LinearExample", solverType);
+
+            // * Create the variables.
+
+            // x and y are continuous non-negative variables.
+            Variable x = solver.MakeNumVar(0.0, double.PositiveInfinity, "x");
+            Variable y = solver.MakeNumVar(0.0, double.PositiveInfinity, "y");
+
+            // * Define the constraints.
+
+            // x + 2y <= 14.
+            Constraint c0 = solver.MakeConstraint(double.NegativeInfinity, 14.0);
+            c0.SetCoefficient(x, 1);
+            c0.SetCoefficient(y, 2);
+
+            // 3x - y >= 0.
+            Constraint c1 = solver.MakeConstraint(0.0, double.PositiveInfinity);
+            c1.SetCoefficient(x, 3);
+            c1.SetCoefficient(y, -1);
+
+            // x - y <= 2.
+            Constraint c2 = solver.MakeConstraint(double.NegativeInfinity, 2.0);
+            c2.SetCoefficient(x, 1);
+            c2.SetCoefficient(y, -1);
+
+            // Define the objective function.
+
+            // Objective function: 3x + 4y.
+            Objective objective = solver.Objective();
+            objective.SetCoefficient(x, 3);
+            objective.SetCoefficient(y, 4);
+            objective.SetMaximization();
+
+          
+
+            Console.WriteLine("Number of variables = " + solver.NumVariables());
+            Console.WriteLine("Number of constraints = " + solver.NumConstraints());
+
+            // * Invoke the solver and display the results.
+            solver.Solve();
+
+            // The value of each variable in the solution.
+            Console.WriteLine("Solution:");
+            Console.WriteLine("x = " + x.SolutionValue());
+            Console.WriteLine("y = " + y.SolutionValue());
+            // The objective value of the solution.
+            Console.WriteLine("Optimal objective value = " +
+                              solver.Objective().Value());
+
+
+
+        }
+    }
+}
